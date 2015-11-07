4e10c2c1563626c2fcbeb694c6679628018affcf
Beehive Robotics <Beehive Robotics@BeehiveRobotics>
2015-11-07 07:51:06 -0700

    start

---
+++ src/edu/wpi/first/wpilibj/templates/JoyStick.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ *
+ * @author Beehive Robotics
+ */
+public class JoyStick
+{
+    public JoyStick()
+    {
+        
+    }
+    
+    Joystick stick = new Joystick(1);
+    public int getValue()
+    {
+        if (stick.getRawButton(1))
+        {
+            return 1;
+        }
+        if (stick.getRawButton(2))
+        {
+            return 2;
+        }
+        if (stick.getRawButton(3))
+        {
+            return 3;
+        }
+        if (stick.getRawButton(4))
+        {
+            return 4;
+        }
+        if (stick.getRawButton(5))
+        {
+            return 5;
+        }
+        if (stick.getRawButton(6))
+        {
+            return 6;
+        }
+        if (stick.getRawButton(7))
+        {
+            return 7;
+        }
+        if (stick.getRawButton(8))
+        {
+            return 8;
+        }
+        if (stick.getRawButton(9))
+        {
+            return 9;
+        }
+        if (stick.getRawButton(10))
+        {
+            return 10;
+        }
+        if (stick.getRawButton(11))
+        {
+            return 11;
+        }
+        return 0;
+    }
+}
---
+++ src/edu/wpi/first/wpilibj/templates/Robot1.java
@@ -0,0 +1,68 @@
+package edu.wpi.first.wpilibj.templates;
+
+//import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+public class Robot1 extends SimpleRobot
+{
+    Launcher l = new Launcher();
+    Victor frontLeft = new Victor(1);
+    Victor frontRight = new Victor(2);
+    Victor backLeft = new Victor(3);
+    Victor backRight = new Victor(4);
+    JoyStick stick = new JoyStick();
+    DoubleSolenoid ds = new DoubleSolenoid(1,2);
+    RobotDrive chassis = new RobotDrive(frontLeft, frontRight, backLeft, backRight);
+    boolean compEnabled = l.compressor.enabled();
+    
+    //code to run on init
+    public void robotInit()
+    {
+        l.compressor.start();
+    }
+    
+    //code to run when autonomous is enabled
+    public void autonomous()
+    {
+        
+    }
+
+    //code that runs from driver station
+    public void operatorControl()
+    {
+        int i = stick.getValue();
+        
+        switch(i)
+        {
+            case 6:
+            {
+                ds.set(DoubleSolenoid.Value.kForward);
+                break;
+            }
+            case 7:
+            {
+                ds.set(DoubleSolenoid.Value.kReverse);
+                break;
+            }
+            case 4:
+            {
+                l.setDegree(30);
+                break;
+            }
+            case 3:
+            {
+                l.setDegree(20);
+                break;
+            }
+            case 5:
+            {
+                l.setDegree(10);
+                break;
+            }
+        }
+    }
+    
+    public void test()
+    {
+        
+    }
+}
